6ba43adde0477fa513a93acafcddee024d923955
csae8092 <p.andorfer@gmail.com>
2026-06-18 14:28:04 +0200

    chore: fetch editions from leopold-brief-data repo

--- shellscripts/fetch_data.sh
+++ shellscripts/fetch_data.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
+rm -rf data
+
+echo "fetching transkriptions from leopold-briefe-data"
+curl -LO https://github.com/loepold-briefe/leopold-briefe-data/archive/refs/heads/main.zip
+unzip main
+mv ./leopold-briefe-data-main/data/ ./data/
+rm -rf ./leopold-briefe-data-main
+rm main.zip
+
 
 echo "fetching entity data from leopold-entities"
-rm -rf data/
 curl -LO https://github.com/loepold-briefe/leopold-entities/archive/refs/heads/main.zip
 unzip main
-mv ./leopold-entities-main/data/ .
+mv ./leopold-entities-main/data/indices ./data/
 rm main.zip
 rm -rf ./leopold-entities-main
 
-echo "fetching transkriptions from leopold-briefe-preprocessing"
-curl -LO https://github.com/loepold-briefe/leopold-briefe-preprocessing/archive/refs/heads/main.zip
-unzip main
-mv ./leopold-briefe-preprocessing-main/data/editions ./data/
-rm -rf ./leopold-briefe-preprocessing-main
-
-echo "fetching transkriptions from leopold-briefe-data"
-
-curl -LO https://github.com/loepold-briefe/leopold-briefe-data/archive/refs/heads/main.zip
-unzip main
-mv ./leopold-briefe-data-main/data/meta ./data/
-rm -rf ./leopold-briefe-data-main
-
-
 echo "fetch imprint"
-rm main.zip
 ./shellscripts/dl_imprint.sh
